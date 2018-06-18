aaad623ec0a3bb4e280a384d2c9f7cef4ed2cb75
sky.oh <sky.oh@nsuslab.com>
2018-06-18 12:41:41 +0900

    prepare external common module

---
+++ common/models/create-expression.ts
@@ -0,0 +1,27 @@
+export function createInstance<T>(param: T): T {
+    const type = typeof param;
+    if (type === 'function') {
+        return createExpr(<any>param);
+    } else {
+        return createExpr(() => param);
+    }
+}
+
+type ParameterExpr<T> = ((param?: T) => T | void) | T;
+export { ParameterExpr };
+
+export function createExpr<T>(expr: ParameterExpr<T>): T {
+    let instance = (<T>{});
+    let result: any;
+    if (typeof expr === 'function') {
+        result = expr(instance);
+    } else {
+        instance = <T>expr;
+    }
+
+    return result === undefined ? instance : <T>result;
+}
+
+export function retrieveInstance(param: any): void {
+    // todo201
+}
