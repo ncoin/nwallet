da433d2da0517f830b57b9c1bd26ad7a1409b0d3
sky.oh <sky.oh@nsuslab.com>
2018-07-02 19:10:07 +0900

    import translate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,18 +12,16 @@ import { IonicStorageModule } from '@ionic/storage';
 
 import { NWalletApp } from './app.component';
 
-
 import { ProvidersModule } from '../providers/providers.module';
-import { HttpClientModule } from '@angular/common/http';
-import { FingerprintAIO } from '@ionic-native/fingerprint-aio'
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FingerprintAIO } from '@ionic-native/fingerprint-aio';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Vibration } from '@ionic-native/vibration';
-
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 BootStrap();
 @NgModule({
-    declarations: [
-        NWalletApp,
-    ],
+    declarations: [NWalletApp],
     imports: [
         BrowserModule,
         HttpClientModule,
@@ -58,19 +56,17 @@ BootStrap();
         ),
         IonicStorageModule.forRoot(),
         ProvidersModule,
+        TranslateModule.forRoot({
+            loader: {
+                provide: TranslateLoader,
+                useFactory: createTranslateLoader,
+                deps: [HttpClient],
+            },
+        }),
     ],
     bootstrap: [IonicApp],
-    entryComponents: [
-        NWalletApp,
-    ],
-    providers: [
-        { provide: ErrorHandler, useClass: IonicErrorHandler },
-        InAppBrowser,
-        SplashScreen,
-        FingerprintAIO,
-        StatusBar,
-        Vibration
-    ],
+    entryComponents: [NWalletApp],
+    providers: [{ provide: ErrorHandler, useClass: IonicErrorHandler }, InAppBrowser, SplashScreen, FingerprintAIO, StatusBar, Vibration],
 })
 export class AppModule {}
 
@@ -78,4 +74,8 @@ function BootStrap() {
     if (env.name === 'prod' || env.name === 'stage') {
         enableProdMode();
     }
+}
+
+export function createTranslateLoader(http: HttpClient) {
+    return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
