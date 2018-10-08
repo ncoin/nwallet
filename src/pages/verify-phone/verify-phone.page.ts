63ff937c4903251922fd4323d48992f3484e6416
sky.oh <sky.oh@nsuslab.com>
2018-10-08 23:54:08 +0900

    verify phone

--- src/pages/verify-phone/verify-phone.page.ts
+++ src/pages/verify-phone/verify-phone.page.ts
@@ -12,7 +12,19 @@ import { ModalNavPage } from '../0.base/modal-nav.page';
     templateUrl: 'verify-phone.page.html'
 })
 export class VerifyPhonePage extends ModalBasePage {
-    public phoneNumber: string;
+    public phoneNumber = '';
+    public countries = [
+        {
+            country: 'ko',
+            code: '82'
+        },
+        {
+            country: 'ko2',
+            code: '822'
+        }
+    ];
+
+    public selectedCountry: { country: string; code: string } = { country: 'de', code: '23' };
 
     constructor(navCtrl: NavController, navParams: NavParams, parent: ModalNavPage, protected logger: LoggerService) {
         super(navCtrl, navParams, parent);
@@ -20,7 +32,11 @@ export class VerifyPhonePage extends ModalBasePage {
     }
 
     public onInput(input: any): void {
-        this.logger.debug(input);
+        if (input === 'delete') {
+            this.phoneNumber = this.phoneNumber.substring(0, this.phoneNumber.length - 1);
+        } else {
+            this.phoneNumber = this.phoneNumber + input;
+        }
     }
     private init(): void {}
 }
