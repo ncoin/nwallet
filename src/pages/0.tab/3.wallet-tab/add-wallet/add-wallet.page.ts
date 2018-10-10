bc4b4e5b594d51f5bb2c3228fc05c62c6fed6349
sky.oh <sky.oh@nsuslab.com>
2018-10-11 01:45:43 +0900

    add-wallets / searchbar (filter, trim)

--- src/pages/0.tab/3.wallet-tab/add-wallet/add-wallet.page.ts
+++ src/pages/0.tab/3.wallet-tab/add-wallet/add-wallet.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, ModalController,  IonicPage,  } from 'ionic-angular';
+import { NavController, ModalController, IonicPage } from 'ionic-angular';
 import { AccountService } from '../../../../providers/account/account.service';
 import { NWAsset } from '../../../../models/nwallet';
 
@@ -10,15 +10,34 @@ import { NWAsset } from '../../../../models/nwallet';
 })
 export class AddWalletPage {
     totalPrice: string;
-
+    public _searchText = '';
     public assets: NWAsset.Item[] = [];
     constructor(public navCtrl: NavController, private accont: AccountService) {
         this.init();
     }
 
-    async init(): Promise<void> {
+    public get searchText(): string {
+        return this._searchText;
+    }
+
+    public set searchText(value: string) {
+        this._searchText = value;
+        this.init();
+        this.onFilterAsset(value);
+    }
+
+    private init(): void {
+        this.assets = [];
         const inventory = this.accont.account_new.inventory;
         this.assets.push(...inventory.assetItems);
+    }
+
+    private onFilterAsset(value: string): void {
+        if (value && value.trim() !== '') {
+            this.assets = this.assets.filter(item => {
+                return item.detail.symbol.trim().toLowerCase().indexOf(value.trim().toLowerCase()) > -1;
+            });
+        }
     }
 
     public onClick_AddAsset(asset: NWAsset.Item): void {
